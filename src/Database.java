7765774bfc5b88a66e0d0acf53bb34ec8beea2c0
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 21:45:40 +0600

    auto commit

--- src/Database.java
+++ src/Database.java
@@ -54,7 +54,30 @@ public class Database {
 		}
 
 	}
-	
+	public void Login1(String queryLogin, String userName, String pass) {
+		try {
+			rs = st.executeQuery(queryLogin);
+			while(rs.next()) {
+				String tableUserName = rs.getString(7);
+				String tablePass = rs.getString(3);
+				if(userName.equals(tableUserName) && pass.equals(tablePass)) {
+					flag =1;
+					break;
+				}
+			}
+			if (flag == 0) {
+				JOptionPane.showMessageDialog(null, "Invalid paswword or username!!");
+			}
+			
+			else {
+				LoginPage lp = new LoginPage();
+				lp.dispose();
+				new Homepage();
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+	}
 
     
 }
