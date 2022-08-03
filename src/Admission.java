729639b9431946138ad317f0bb6978d3f2036974
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:50:07 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -31,6 +31,13 @@ public class Admission extends JFrame {
         headerpanel.add(imgLabel);
         add(headerpanel);
 
+        JPanel inputpanel1=new JPanel();
+		inputpanel1.setBackground(new Color(93,90,100));
+        inputpanel1.setBounds(300, 150, 350, 50);
+		//inputpanel1.setLayout(null);
+		JLabel imgLabel2 = new JLabel(new ImageIcon(this.getClass().getResource("/image/Registration.png")));
+		inputpanel1.add(imgLabel2);
+
         JLabel Homebtn=new JLabel("New Student Admission");
         Homebtn.setBounds(0,150,1000,30);
 	    Homebtn.setBackground(new Color(69,90,100));
