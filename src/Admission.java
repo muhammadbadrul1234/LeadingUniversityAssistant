b41e38633e2a92d5a0fa19b7293fff3e53140d6f
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:15:29 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -33,6 +33,18 @@ public class Admission extends JFrame {
         headerpanel.add(imgLabel);
         add(headerpanel);
 
+        JPanel headerpane2 = new JPanel();
+        //headerpanel.setBackground(new Color(93,130,250));
+        headerpane.setBounds(0, 0, 1000, 150);
+
+        JLabel headerTxt = new JLabel("Welcome to Leading University Portal");
+        headerTxt.setFont((new Font("Segoe UI", Font.BOLD, 30)));
+        headerTxt.setForeground(Color.WHITE);
+        JLabel imgLabel = new JLabel(new ImageIcon(this.getClass().getResource("/image/header.png")));
+
+        headerpanel.add(imgLabel);
+        add(headerpanel);
+
         JFrame f;
         JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,id16,id17,lab,lab1,lab2;
         JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
