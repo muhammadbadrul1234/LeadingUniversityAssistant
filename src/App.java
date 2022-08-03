e6c0724bd5be2e46d630d1e85fb63ec1e23e95b8
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 22:26:58 +0600

    auto commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
        //new LoginPage();
-       new Register();
-        new 
+       //new Register();
+       new Homepage();
+        
     }
 }
