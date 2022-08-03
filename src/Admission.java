d30dd5496447db04a738680d2e43a87e0a4a2455
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 03:38:32 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -348,7 +348,7 @@ public class Admission extends JFrame {
             String k = t11.getText();
             String l = (String)c1.getSelectedItem();
                 String m = (String) c2.getSelectedItem();
-            if(a=/0){
+            if(a==null){
 
             }
             String insertQuery = "INSERT INTO `student`"
