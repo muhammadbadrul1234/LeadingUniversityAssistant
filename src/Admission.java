98a6c0d085797f365cba0b789386bcee5d62b249
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:52:49 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -32,12 +32,12 @@ public class Admission extends JFrame {
         add(headerpanel);
 
         JLabel Homebtn=new JLabel("New Student Admission");
-        Homebtn.setBounds(0,150,1000,30);
+        Homebtn.setBounds(500,150,1000,30);
 	    //Homebtn.setBackground(new Color(69,90,100));
 	    Homebtn.setFont(labelFont);
 	    Homebtn.setForeground(Color.WHITE);
 	    //Homebtn.setBorder(new LineBorder(new Color(69,90,100)));
-	    Homebtn.setFocusable(false);
+	   // Homebtn.setFocusable(false);
         
 
         JPanel inputpanel1=new JPanel();
