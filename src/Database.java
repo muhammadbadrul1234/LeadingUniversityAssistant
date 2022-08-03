ab6cdead4dd62e9a3a658538b0c940ebe6145354
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 17:36:28 +0600

    auto commit

--- src/Database.java
+++ src/Database.java
@@ -33,6 +33,7 @@ public class Database {
 				JOptionPane.showMessageDialog(null, "Invalid paswword or username!!");
 			}
 			else {
+				dispose();
 				new Homepage();
 			}
 		} catch (Exception e) {
