7fd9fa5f22cbc5bf1b3c2676461fcc22c65e716b
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:59:11 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -22,6 +22,7 @@ public class Admission extends JFrame {
 
         Font labelFont = ((new Font("Segoe UI", Font.CENTER_BASELINE, 16)));
         Font labelFont2 = ((new Font("Segoe UI", Font.ROMAN_BASELINE, 20)));
+        Font labelFont3 = ((new Font("Segoe UI", Font.CENTER_BASELINE, 16)));
         JPanel headerpanel = new JPanel();
         //headerpanel.setBackground(new Color(93,130,250));
         headerpanel.setBounds(0, 0, 1000, 150);
