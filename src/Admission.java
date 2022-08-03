86c9b51f016eccec7dfd5a145251252f01f74f0b
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 03:06:59 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -329,7 +329,7 @@ public class Admission extends JFrame {
             String k = t11.getText();
             String l = (String)c1.getSelectedItem();
             String m = (String) c2.getSelectedItem();
-            String insertQuery = "insert into student values('"+a+"','"+bb+"','"+c+"','"+d+"','"+ee+"','"+ff+"','"+g+"','"+h+"','"+i+"','"+j+"','"+k+"','"+l+"','"+m+"')";
+            String insertQuery = "insert into student('"+a+"','"+bb+"','"+c+"','"+d+"','"+ee+"','"+ff+"','"+g+"','"+h+"','"+i+"','"+j+"','"+k+"','"+l+"','"+m+"')";
 				
 				Database db = new Database();
 				db.insertRegister(insertQuery);
