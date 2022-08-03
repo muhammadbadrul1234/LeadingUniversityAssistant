8ea03fce73d713c571292cc2e45491bfb29827e1
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 22:00:47 +0600

    auto commit

--- src/Database.java
+++ src/Database.java
@@ -76,6 +76,7 @@ public class Database {
 				JOptionPane.showMessageDialog(null, "Password is: + (x) ");
 				//JOptionPane.showMessageDialog (null, x, "Title", JOptionPane.INFORMATION_MESSAGE);
 			}
+			System.out.println(x);
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 		}
