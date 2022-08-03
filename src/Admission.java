108545dddf625429fefa15e90ea5e44c00052cf9
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 02:51:51 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -311,38 +311,8 @@ public class Admission extends JFrame {
 			}
 		});
 
-        public void actionPerformed(ActionEvent ae){
         
-        String a = t1.getText();
-        String bb = t2.getText();
-        String c = t3.getText();
-        String d = t4.getText();
-        String e = t5.getText();
-        String ff = t6.getText();
-        String g = t7.getText();
-        String h = t8.getText();
-        String i = t9.getText();
-        String j = t10.getText();
-        String k = t11.getText();
-        String l = (String)c1.getSelectedItem();
-        String m = (String)c2.getSelectedItem();
-        
-        if(ae.getSource() == b){
-            try{
-                conn cc = new conn();
-                String q = "insert into student values('"+a+"','"+bb+"','"+c+"','"+d+"','"+e+"','"+ff+"','"+g+"','"+h+"','"+i+"','"+j+"','"+k+"','"+l+"','"+m+"')";
-                cc.s.executeUpdate(q);
-                JOptionPane.showMessageDialog(null,"Student Details Inserted Successfully");
-                f.setVisible(false);
-            }catch(Exception ee){
-                System.out.println("The error is:"+ee);
-            }
-        }else if(ae.getSource() == b1){
-            f.setVisible(false);
-            new Project().setVisible(true);
-            
-        }
-    }
+    
         
 
 
