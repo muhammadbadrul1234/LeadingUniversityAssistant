5b13967c7dae4a8ea70f7ddbefea8b5ace94e4c5
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 17:43:52 +0600

    auto commit

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
-import database;
+import LoginPage.LoginPage;
 public class Database {
     private Connection con;
 	private Statement st;
