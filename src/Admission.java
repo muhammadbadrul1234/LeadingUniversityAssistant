e12d2e4dedb525a4d840ba542f9e112726dda41c
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 02:41:45 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -46,7 +46,94 @@ public class Admission extends JFrame {
         inputpanel2.setBounds(0, 150, 1000, 50);
         inputpanel2.setLayout(null);
         add(Homebtn);
-        add(inputpanel1);
+        add(inputpanel2);
+
+         JPanel inputpanel1=new JPanel();
+	//inputpanel1.setBackground(new Color(112,128,144));
+        inputpanel1.setBounds(0, 150, 1000, 30);
+		inputpanel1.setLayout(null);
+        
+        
+        JButton Homebtn=new JButton("Home");
+	    Homebtn.setBounds(0,0,100,30);
+	    Homebtn.setBackground(new Color(69,90,100));
+	    Homebtn.setFont(labelFont);
+	    Homebtn.setForeground(Color.WHITE);
+	    Homebtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Homebtn.setFocusable(false);
+        inputpanel1.add(Homebtn);
+        
+        JButton Admissionbtn=new JButton("Admission");
+	    Admissionbtn.setBounds(100,0,100,30);
+	    Admissionbtn.setBackground(new Color(69,90,100));
+	    Admissionbtn.setFont(labelFont);
+	    Admissionbtn.setForeground(Color.WHITE);
+	    Admissionbtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Admissionbtn.setFocusable(false);
+        inputpanel1.add(Admissionbtn);
+        
+        JButton contacButton=new JButton("Contact");
+	    contacButton.setBounds(200,0,100,30);
+	    contacButton.setBackground(new Color(69,90,100));
+	    contacButton.setFont(labelFont);
+	    contacButton.setForeground(Color.WHITE);
+	    contacButton.setBorder(new LineBorder(new Color(69,90,100)));
+	    contacButton.setFocusable(false);
+        inputpanel1.add(contacButton);
+        
+        JButton FormBtn=new JButton("Forms");
+	    FormBtn.setBounds(300,0,100,30);
+	    FormBtn.setBackground(new Color(69,90,100));
+	    FormBtn.setFont(labelFont);
+	    FormBtn.setForeground(Color.WHITE);
+	    FormBtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    FormBtn.setFocusable(false);
+        inputpanel1.add(FormBtn);
+        
+        JButton noticebButton=new JButton("Notice");
+	    noticebButton.setBounds(400,0,100,30);
+	    noticebButton.setBackground(new Color(69,90,100));
+	    noticebButton.setFont(labelFont);
+	    noticebButton.setForeground(Color.WHITE);
+	    noticebButton.setBorder(new LineBorder(new Color(69,90,100)));
+	    noticebButton.setFocusable(false);
+        inputpanel1.add(noticebButton);
+        
+        JButton Deptsbtn=new JButton("Departments");
+	    Deptsbtn.setBounds(500,0,120,30);
+	    Deptsbtn.setBackground(new Color(69,90,100));
+	    Deptsbtn.setFont(labelFont);
+	    Deptsbtn.setForeground(Color.WHITE);
+	    Deptsbtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Deptsbtn.setFocusable(false);
+        inputpanel1.add(Deptsbtn);
+        
+        JButton Librarybtn=new JButton("Library");
+	    Librarybtn.setBounds(620,0,100,30);
+	    Librarybtn.setBackground(new Color(69,90,100));
+	    Librarybtn.setFont(labelFont);
+	    Librarybtn.setForeground(Color.WHITE);
+	    Librarybtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Librarybtn.setFocusable(false);
+        inputpanel1.add(Librarybtn);
+        
+        JButton Resultbtn=new JButton("Result");
+	    Resultbtn.setBounds(720,0,70,30);
+	    Resultbtn.setBackground(new Color(69,90,100));
+	    Resultbtn.setFont(labelFont);
+	    Resultbtn.setForeground(Color.WHITE);
+	    Resultbtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Resultbtn.setFocusable(false);
+        inputpanel1.add(Resultbtn);
+        
+        JButton Regbtn=new JButton("Semester Registration");
+	    Regbtn.setBounds(790,0,195,30);
+	    Regbtn.setBackground(new Color(69,90,100));
+	    Regbtn.setFont(labelFont);
+	    Regbtn.setForeground(Color.WHITE);
+	    Regbtn.setBorder(new LineBorder(new Color(69,90,100)));
+	    Regbtn.setFocusable(false);
+        inputpanel1.add(Regbtn);
 
         
 
