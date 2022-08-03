3e9cad420ec6f169d5819952a6c3c213ffc7068a
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:02:25 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -33,6 +33,13 @@ public class Admission extends JFrame {
         headerpanel.add(imgLabel);
         add(headerpanel);
 
+        JFrame f;
+        JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,id16,id17,lab,lab1,lab2;
+        JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
+        JButton b,b1,b2,b3;
+        JComboBox c1, c2;
+        
+
 
 
 
