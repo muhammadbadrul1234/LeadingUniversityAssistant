9256ca23fd9e4a857831caa4bd27a3ec50a793c1
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 03:03:17 +0600

    auto commit

--- src/Database.java
+++ src/Database.java
@@ -54,24 +54,25 @@ public class Database {
 		}
 
 	}
+
 	public void Login1(String queryLogin, String userName, String pass) {
 		try {
-			
+
 			rs = st.executeQuery(queryLogin);
 			String x;
-			while(rs.next()) {
+			while (rs.next()) {
 				String tableUserName = rs.getString(7);
 				String tablePass = rs.getString(3);
 
-				if(userName.equals(tableUserName) && pass.equals(tablePass)) {
-					flag =1;
+				if (userName.equals(tableUserName) && pass.equals(tablePass)) {
+					flag = 1;
 					break;
 				}
 			}
 			if (flag == 0) {
 				JOptionPane.showMessageDialog(null, "Data Not Found");
 			}
-			
+
 			else {
 				JOptionPane.showMessageDialog(null, "Password is: aa ");
 				//JOptionPane.showMessageDialog (null, x, "Title", JOptionPane.INFORMATION_MESSAGE);
@@ -80,6 +81,19 @@ public class Database {
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 		}
+	}
+	
+	public void aRegister(String insertQuesry) {
+		try {
+			st.executeUpdate(insertQuesry);
+			JOptionPane.showMessageDialog(null, "You have successfully registered!");
+		}
+
+		catch (Exception e) {
+
+			JOptionPane.showMessageDialog(null, "Registration Not Completed");
+		}
+
 	}
 
     
