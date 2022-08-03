f0055324af2d92ebdd39ab3094c45923f5e7d71b
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 01:43:44 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -30,6 +30,7 @@ public class Admission extends JFrame {
         headerpanel.add(imgLabel);
         add(headerpanel);
 
+        JLabel Homebtn=new JLabel("Welcome to Leading University Portal");
         Homebtn.setBounds(0,0,100,30);
 	    Homebtn.setBackground(new Color(69,90,100));
 	    Homebtn.setFont(labelFont);
