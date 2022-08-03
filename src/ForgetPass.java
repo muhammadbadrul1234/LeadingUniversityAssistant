3a87d2ff1eda030c049539e17b66cf2f99991bd0
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-03 21:35:55 +0600

    auto commit

--- src/ForgetPass.java
+++ src/ForgetPass.java
@@ -36,7 +36,7 @@ public class ForgetPass extends JFrame{
 
 		JPanel inputpanel1=new JPanel();
 		//inputpanel1.setBackground(new Color(93,130,250));
-        inputpanel1.setBounds(300, 150, 280, 50);
+        inputpanel1.setBounds(335, 150, 280, 50);
 		//inputpanel1.setLayout(null);
 		JLabel imgLabel2 = new JLabel(new ImageIcon(this.getClass().getResource("/image/Forgot.png")));
 		inputpanel1.add(imgLabel2);
