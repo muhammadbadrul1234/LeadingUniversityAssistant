ffbc90f574a91d7e28fbea4577be9e8186ad2402
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-04 00:59:43 +0600

    auto commit

--- src/Admission.java
+++ src/Admission.java
@@ -1,3 +1,46 @@
-public class Admission extends {
-    
-}
+import javax.swing.JFrame;
+import javax.swing.border.LineBorder;
+import javax.xml.crypto.Data;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+
+public class Admission extends JFrame {
+    public Admission() {
+        super("Leading University");
+        
+        ImageIcon icon;
+        icon = new ImageIcon(this.getClass().getResource("/image/logo.png"));
+        this.setIconImage(icon.getImage());
+
+        setSize(1000,800);
+        setLocationRelativeTo(null);
+		setDefaultCloseOperation(3);
+		setLayout(null);
+        
+        Font labelFont=((new Font("Segoe UI",Font.CENTER_BASELINE,16)));
+	    JPanel headerpanel=new JPanel();
+	    //headerpanel.setBackground(new Color(93,130,250));
+		headerpanel.setBounds(0, 0, 1000, 150);
+		
+
+	    
+	    JLabel headerTxt=new JLabel("Welcome to Leading University Portal");
+		headerTxt.setFont((new Font("Segoe UI", Font.BOLD, 30)));
+		headerTxt.setForeground(Color.WHITE);
+		JLabel imgLabel = new JLabel(new ImageIcon(this.getClass().getResource("/image/header.png")));
+		
+		headerpanel.add(imgLabel);
+		add(headerpanel);
+		
+	
+        JPanel inputpanel1=new JPanel();
+		
+
+        inputpanel1.setBounds(0, 90, 350, 700);
+
+		JLabel imgLabel2 = new JLabel(new ImageIcon(this.getClass().getResource("/image/lu.png")));
+		inputpanel1.add(imgLabel2);
+		add(inputpanel1);
