e7ef68069a53eedb0776750bac6c2cdeb2a659af
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:21:12 +0600

    Commit By Muhammad Badrul

--- src/Demo.java
+++ src/Demo.java
@@ -55,6 +55,69 @@ public class Demo extends JFrame {
                 mb.add(master);
                 setJMenuBar(mb);
                          
+        public void actionPerformed(ActionEvent ae){
+        String msg = ae.getActionCommand();
+        if(msg.equals("New Student Admission")){
+            new AddStudent().f.setVisible(true);
+            
+        }else if(msg.equals("New Faculty")){
+            new AddTeacher().f.setVisible(true);
+            
+        }else if(msg.equals("Student Details")){
+            new StudentDetails().setVisible(true);
+            
+        }else if(msg.equals("Teacher Details")){
+            new TeacherDetails().setVisible(true);
+           
+        }
+        else if(msg.equals("Update Students")){
+            new UpdateStudent().f.setVisible(true);
+           
+        }
+        else if(msg.equals("Update Teachers")){
+            new UpdateTeacher().f.setVisible(true);
+           
+        }
+        else if(msg.equals("Fee Structure")){
+            new FeeStructure().setVisible(true);
+           
+        }
+        else if(msg.equals("Student Fee Form")){
+            new StudentFeeForm().setVisible(true);
+           
+        }
+        else if(msg.equals("Notepad")){
+            try{
+                Runtime.getRuntime().exec("notepad.exe");
+            }catch(Exception e){ }
+        }else if(msg.equals("Calculator")){
+            try{
+                Runtime.getRuntime().exec("calc.exe");
+            }catch(Exception e){ }
+        }else if(msg.equals("Web Browser")){
+            
+            try{
+                Runtime.getRuntime().exec("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+            }catch(Exception e){ }
+        }else if(msg.equals("Exit")){
+            System.exit(0);
+        }else if(msg.equals("About Us")){
+            new AboutUs().setVisible(true);
+        }else if(msg.equals("Student Attendance")){
+            new StudentAttendance().setVisible(true);
+        }else if(msg.equals("Teacher Attendance")){
+            new TeacherAttendance().setVisible(true);
+        }else if(msg.equals("Student Attendance Detail")){
+            new StudentAttendanceDetail().setVisible(true);
+        }else if(msg.equals("Teacher Attendance Detail")){
+            new TeacherAttendanceDetail().setVisible(true);
+        }else if(msg.equals("Examination Details")){
+            new ExaminationDetails().setVisible(true);
+        }else if(msg.equals("Enter Marks")){
+            new EnterMarks().setVisible(true);
+        }
+        
+    }
 
 
 
