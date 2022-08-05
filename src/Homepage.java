294b9387a926a5ea881a46590d0d0f70a7ea8c09
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:12:06 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -67,6 +67,7 @@ public class Homepage extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(new Color(69, 90, 100));
 		menuBar.setBorder(new LineBorder(new Color(69, 90, 100)));
+		menuvar.setBounds
 		menuBar.setForeground(Color.WHITE);
 		inputpanel1.add(menuBar);
 
