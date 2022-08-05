f222f465866ff10ad2a09819f8aa0d4e8912bd53
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-05 23:49:27 +0600

    Commit By Muhammad Badrul

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -126,7 +126,7 @@ public class LoginPage extends JFrame {
 	    Resultbtn.setFocusable(false);
         inputpanel2.add(Resultbtn);
         
-        JButton Regbtn=new JButton("Register");
+        JButton Regbtn=new JButton("Return");
 	    Regbtn.setBounds(1020,0,195,30);
 	    Regbtn.setBackground(new Color(69,90,100));
 	    Regbtn.setFont(labelFont);
