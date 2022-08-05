592bc06be9ce3c6c74b58557331dbaa5479700fe
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 03:40:15 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -128,7 +128,7 @@ public class Homepage extends JFrame {
 	    Database.setBorder(new LineBorder(new Color(69,90,100)));
 	    Database.setFocusable(false);
         Database.setBackground(new Color(69,90,100));
-        Database.setBounds(230, 0, 130, 30);
+        Database.setBounds(260, 0, 130, 30);
         Database.setUI(new BasicComboBoxUI() {
             @Override
             protected ComboPopup createPopup() {
