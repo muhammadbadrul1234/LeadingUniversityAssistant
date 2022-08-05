801efda0dbf65a767488628cd5142c3aef6ab013
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 01:13:17 +0600

    Commit By Muhammad Badrul

--- src/AboutUs.java
+++ src/AboutUs.java
@@ -126,51 +126,51 @@ public class AboutUs extends JFrame {
         inputpanel1.add(Regbtn);
 
 
-		  setBackground(new Color(173, 216, 230));
-            //setBounds(500, 250, 700, 500);
+		//   setBackground(new Color(173, 216, 230));
+        //     //setBounds(500, 250, 700, 500);
 		
-			contentPane = new JPanel();
+		// 	contentPane = new JPanel();
 			
-            setContentPane(contentPane);
-           contentPane.setLayout(null);
+        //     setContentPane(contentPane);
+        //    contentPane.setLayout(null);
 
             
 
 
-            JLabel l3 = new JLabel("University");
-            l3.setForeground(new Color(0, 250, 154));
-            l3.setFont(new Font("Courier new", Font.BOLD, 30));
-            l3.setBounds(140, 200, 200, 55);
-            contentPane.add(l3);
+        //     JLabel l3 = new JLabel("University");
+        //     l3.setForeground(new Color(0, 250, 154));
+        //     l3.setFont(new Font("Courier new", Font.BOLD, 30));
+        //     l3.setBounds(140, 200, 200, 55);
+        //     contentPane.add(l3);
 
-            JLabel l4 = new JLabel("Mangement System");
-            l4.setForeground(new Color(127, 255, 0));
-            l4.setFont(new Font("Courier New", Font.BOLD, 30));
-            l4.setBounds(70, 290, 405, 40);
-            contentPane.add(l4);
-
-
-            JLabel l6 = new JLabel("Developed By : IT SOURCE CODE");
-            l6.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l6.setBounds(70, 198, 600, 35);
-            contentPane.add(l6);
-
-            JLabel l8 = new JLabel("Contact : suarez081119@gmail.com");
-            l8.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            l8.setBounds(70, 290, 600, 34);
-            contentPane.add(l8);
-
-            JLabel l9 = new JLabel("Education - B.Tech (Computer Science)");
-            l9.setFont(new Font("Trebuchet MS", Font.BOLD , 20));
-            l9.setBounds(70, 320, 600, 34);
-            contentPane.add(l9);
+        //     JLabel l4 = new JLabel("Mangement System");
+        //     l4.setForeground(new Color(127, 255, 0));
+        //     l4.setFont(new Font("Courier New", Font.BOLD, 30));
+        //     l4.setBounds(70, 290, 405, 40);
+        //     contentPane.add(l4);
 
 
-            JLabel l10 = new JLabel("Phone - +639272777334");
-            l10.setForeground(new Color(47, 79, 79));
-            l10.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 18));
-            l10.setBounds(70, 400, 600, 34);
-            contentPane.add(l10);
+        //     JLabel l6 = new JLabel("Developed By : IT SOURCE CODE");
+        //     l6.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+        //     l6.setBounds(70, 198, 600, 35);
+        //     contentPane.add(l6);
+
+        //     JLabel l8 = new JLabel("Contact : suarez081119@gmail.com");
+        //     l8.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+        //     l8.setBounds(70, 290, 600, 34);
+        //     contentPane.add(l8);
+
+        //     JLabel l9 = new JLabel("Education - B.Tech (Computer Science)");
+        //     l9.setFont(new Font("Trebuchet MS", Font.BOLD , 20));
+        //     l9.setBounds(70, 320, 600, 34);
+        //     contentPane.add(l9);
+
+
+        //     JLabel l10 = new JLabel("Phone - +639272777334");
+        //     l10.setForeground(new Color(47, 79, 79));
+        //     l10.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 18));
+        //     l10.setBounds(70, 400, 600, 34);
+        //     contentPane.add(l10);
                 
 		
 
