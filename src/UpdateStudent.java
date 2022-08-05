101659c9a21810549447d13ac8b3eeb82e0afefb
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 05:52:34 +0600

    Commit By Muhammad Badrul

--- src/UpdateStudent.java
+++ src/UpdateStudent.java
@@ -50,13 +50,13 @@ public class UpdateStudent extends JFrame {
         //inputpanel1.setBackground(new Color(112,128,144));
         inputpanel1.setBounds(0, 150, 1000, 30);
         inputpanel1.setLayout(null);
-JFrame f;
+        JFrame f;
         JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,id16,id17,lab,lab1,lab2;
         JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
         JButton b,b1,b2,b3;
         JComboBox c1, c2;
         String id_emp;
-        JComboBox c1, c2;
+        //JComboBox c1, c2;
 
         id8 = new JLabel("Update Student Details:");
         id8.setBounds(50, 10, 500, 50);
