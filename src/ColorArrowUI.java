a18bd8504bbf9507e3bdc96e633c436bfd78e4f5
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:46:45 +0600

    Commit By Muhammad Badrul

--- src/ColorArrowUI.java
+++ src/ColorArrowUI.java
@@ -1,3 +1,15 @@
-java.lang.Object
-  extended by javax.swing.plaf.ComponentUI
-      extended by javax.swing.plaf.ComboBoxUI
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+class ColorArrowUI extends BasicComboBoxUI {
+
+    public static ComboBoxUI createUI(JComponent c) {
+        return new ColorArrowUI();
+    }
+
+    @Override protected JButton createArrowButton() {
+        return new BasicArrowButton(
+            BasicArrowButton.SOUTH,
+            Color.cyan, Color.magenta,
+            Color.yellow, Color.blue);
+    }
+}
