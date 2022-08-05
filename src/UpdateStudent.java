0a72d2a1608bde7f953b9afbf261ddf436ba23db
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 05:44:50 +0600

    Commit By Muhammad Badrul

--- src/UpdateStudent.java
+++ src/UpdateStudent.java
@@ -62,7 +62,11 @@ public class UpdateStudent extends JFrame {
         
 
 
-
+JFrame f;
+    JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,lab,lab1,lab2;
+    JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14;
+    JButton b,b1,b2; 
+    String id_emp;
 
         id8 = new JLabel("Update Student Details:");
         id8.setBounds(50,10,500,50);
