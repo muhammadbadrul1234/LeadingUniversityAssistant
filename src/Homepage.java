7fd044ee90165761a02ec4dd29c7fe5a49e702d3
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 03:46:40 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -167,36 +167,7 @@ public class Homepage extends JFrame {
 		
 		
 		
-		//add(inputpanel1);
-
-		String dtbs[] = {"Data Sheet","Student Details","Teacher Details"," Employee Details","Student Attendance","Teacher Attendence"," Employee Attendance"};
-        Database = new JComboBox(dtbs);
-        // c1.setUI(ColorArrowUI.createUI(c1));
-       
-        
-        Database.setName("Badrul");
-        Database.setBackground(new Color(69,90,100));
-	   	Database.setFont(labelFont);
-	    Database.setForeground(Color.WHITE);
-	    Database.setBorder(new LineBorder(new Color(69,90,100)));
-	    Database.setFocusable(false);
-        Database.setBackground(new Color(69,90,100));
-        Database.setBounds(330, 0, 130, 30);
-        Database.setUI(new BasicComboBoxUI() {
-            @Override
-            protected ComboPopup createPopup() {
-                BasicComboPopup basicComboPopup = new BasicComboPopup(comboBox);
-                        basicComboPopup.setBorder(new LineBorder(new Color(69, 90, 100)));
-                basicComboPopup.setBackground(new Color(69,90,100));
-                return basicComboPopup;
-            }
-		});
-
-		
-		
-		inputpanel1.add(Database);
-		inputpanel1.add(Update);
-		inputpanel1.add(Manage);
+		//add(inputpanel1
 
        
 
