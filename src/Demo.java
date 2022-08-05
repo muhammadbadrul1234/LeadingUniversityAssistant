9ef65f10589ee6de6755837485d802d6888a9857
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:33:39 +0600

    Commit By Muhammad Badrul

--- src/Demo.java
+++ src/Demo.java
@@ -19,7 +19,11 @@ public class Demo extends JFrame {
         ImageIcon icon;
         icon = new ImageIcon(this.getClass().getResource("/image/logo.png"));
         this.setIconImage(icon.getImage());
-
+ JFrame f;
+        JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,id16,id17,lab,lab1,lab2;
+        JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
+        JButton b,b1,b2,b3;
+        JComboBox c1, c2;
         setSize(1250,800);
         setLocationRelativeTo(null);
 		setDefaultCloseOperation(3);
