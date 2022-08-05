05dda4e5be7a1cf8f74769b1b67a45fe6c042ad0
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 00:21:41 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -40,6 +40,7 @@ public class Homepage extends JFrame {
 	//inputpanel1.setBackground(new Color(112,128,144));
         inputpanel1.setBounds(0, 150, 1250, 30);
 		inputpanel1.setLayout(null);
+		input
 
 
         JButton Homebtn=new JButton("Home");
