640284fcf6ef2deeb51f4c4b762d041f1378e9a9
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 05:37:07 +0600

    Commit By Muhammad Badrul

--- src/FrontEndClass/ZFPSyndicate.java
+++ src/FrontEndClass/ZFPSyndicate.java
@@ -20,7 +20,7 @@ import javax.swing.plaf.ComboBoxUI;
 public class ZFPSyndicate extends JFrame {
     JScrollPane jp;
     JLabel img;
-   ZFPSyndicate(){
+   public ZFPSyndicate(){
     super("Leading University");
         ImageIcon icon;
         icon = new ImageIcon(this.getClass().getResource("/image/logo.png"));
