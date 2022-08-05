7537671800ec11459e6d202c33f0a2049aa99d7f
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:59:33 +0600

    Commit By Muhammad Badrul

--- src/ColorArrowUI.java
+++ src/ColorArrowUI.java
@@ -7,17 +7,14 @@ import java.awt.*;
 
 class ColorArrowUI extends BasicComboBoxUI {
 
-
     public static ComboBoxUI createUI(JComponent c) {
         return new ColorArrowUI();
     }
 
     @Override protected JButton createArrowButton() {
         return new BasicArrowButton(
-                BasicArrowButton.SOUTH,
-            Color.#455A64,
-            Color.RGBtoHSB(69, 90, 100, ),
-            , Color.magenta,
+            BasicArrowButton.SOUTH,
+            Color.cyan, Color.magenta,
             Color.yellow, Color.blue);
     }
 }
