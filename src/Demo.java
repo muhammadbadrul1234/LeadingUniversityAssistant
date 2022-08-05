7665516e371a87aebf947c3e5491fa0c45ce0488
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:18:10 +0600

    Commit By Muhammad Badrul

--- src/Demo.java
+++ src/Demo.java
@@ -13,59 +13,18 @@ public class Demo extends JFrame {
 
     Demo() {
         super("Leading University");
+        
         ImageIcon icon;
         icon = new ImageIcon(this.getClass().getResource("/image/logo.png"));
         this.setIconImage(icon.getImage());
 
-         Font labelFont=((new Font("Segoe UI",Font.CENTER_BASELINE,16)));
-	    JPanel headerpanel=new JPanel();
-	    //headerpanel.setBackground(new Color(93,130,250));
-        headerpanel.setBounds(0, 0, 1250, 150);
-        headerpanel.setBackground(new Color(69,90,100));
-		
-	    
-	    JLabel headerTxt=new JLabel("Welcome to Leading University Portal");
-		headerTxt.setFont((new Font("Segoe UI", Font.BOLD, 30)));
-		headerTxt.setForeground(Color.WHITE);
-		JLabel imgLabel = new JLabel(new ImageIcon(this.getClass().getResource("/image/header.png")));
-		
-		headerpanel.add(imgLabel);
-        add(headerpanel);
-        JPanel inputpanel1=new JPanel();
-	    inputpanel1.setBackground(new Color(69,90,100));
-        inputpanel1.setBounds(0, 150, 1250, 30);
-		inputpanel1.setLayout(null);
-    
-        JButton Resultbtn=new JButton("Login");
-	    Resultbtn.setBounds(950,0,70,30);
-	    Resultbtn.setBackground(new Color(69,90,100));
-	    Resultbtn.setFont(labelFont);
-	    Resultbtn.setForeground(Color.WHITE);
-	    Resultbtn.setBorder(new LineBorder(new Color(69,90,100)));
-	    Resultbtn.setFocusable(false);
-        inputpanel1.add(Resultbtn);
-        
-        JButton Regbtn=new JButton("Register");
-	    Regbtn.setBounds(1020,0,195,30);
-	    Regbtn.setBackground(new Color(69,90,100));
-	    Regbtn.setFont(labelFont);
-	    Regbtn.setForeground(Color.WHITE);
-	    Regbtn.setBorder(new LineBorder(new Color(69,90,100)));
-	    Regbtn.setFocusable(false);
-        inputpanel1.add(Regbtn);
-        
-        setSize(1250, 800);
+        setSize(1250,800);
         setLocationRelativeTo(null);
 		setDefaultCloseOperation(3);
 		setLayout(null);
-        img = new JLabel(new ImageIcon("fp.png"));
-        jp = new JScrollPane(img);
-        jp.setBounds(00, 180, 1234, 580);
-        jp.setForeground(Color.WHITE);
-        jp.setViewportView(img);
-        add(jp);
 
-        add(inputpanel1);
+
+       // add(inputpanel1);
         setVisible(true);
 
     }
