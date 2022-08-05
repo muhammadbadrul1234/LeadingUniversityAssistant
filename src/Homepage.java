1df308a523f9b78626b10ccad943ed5cef73301e
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 03:29:49 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -91,7 +91,7 @@ public class Homepage extends JFrame {
                 return basicComboPopup;
             }
         });
-		inputpanel1.add(Manage);
+		inputpanel1.add(Update);
 		
 		String Updt[] = {"Update","Student"," Teacher"," Employee"};
         Update = new JComboBox(Man);
