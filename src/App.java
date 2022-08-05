eab5f905ffe2eeefc2d59a5995d6378fb4ef5a54
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 03:14:40 +0600

    Commit By Muhammad Badrul

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 public class App {
   public static void main(String[] args) throws Exception {
-      new FrontPage(); 
+      //new FrontPage(); 
        //new LoginPage();
       // new Register();
     new Homepage();
       // new Admission();
       // new AddEmployee();
      // new AboutUs();
-     new Demo();
+     //new Demo();
       //new ForgetPass();
         
     }
