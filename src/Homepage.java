cd2f6de136c2545ee4e0b790083c54432c782bee
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 03:22:04 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -69,7 +69,7 @@ public class Homepage extends JFrame {
 	    Homebtn.setFocusable(false);
 		inputpanel1.add(Homebtn);
 		
-		String Man[] = {"B.Tech","BBA","BCA","Bsc","Msc","MBA","MCA","BA","BCom"};
+		String Man[] = {"Admission","BBA","BCA","Bsc","Msc","MBA","MCA","BA","BCom"};
         Manage = new JComboBox(course);
         // c1.setUI(ColorArrowUI.createUI(c1));
        
