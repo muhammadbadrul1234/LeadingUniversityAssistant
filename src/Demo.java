7d9b892327bbe9b32a0358b99ad4664d9bd62078
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 02:26:55 +0600

    Commit By Muhammad Badrul

--- src/Demo.java
+++ src/Demo.java
@@ -26,45 +26,7 @@ public class Demo extends JFrame {
 		setDefaultCloseOperation(3);
         setLayout(null);
 
-        JMenuBar mb = new JMenuBar();
-        mb.setBounds(150, 150, 1250, 30);
-        mb.setBackground(new Color(69, 90, 100));
-        mb.setBorder(new LineBorder(new Color(69, 90, 100)));
-        
-        JMenu master = new JMenu("Master");
-        JMenuItem m1 = new JMenuItem("New Faculty");
-        JMenuItem m2 = new JMenuItem("New Student Admission");
-        master.setForeground(Color.BLUE);
-
-        m1.setFont(new Font("monospaced",Font.BOLD,16));
-        // ImageIcon icon1 = new ImageIcon(ClassLoader.getSystemResource("university/management/system/icons/icon1.png"));
-        // Image image1 = icon1.getImage().getScaledInstance(25, 25,Image.SCALE_DEFAULT);
-        // m1.setIcon(new ImageIcon(image1));
-        // m1.setMnemonic('A');
-        // m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
-        m1.setBackground(Color.WHITE);
-        
-        m2.setFont(new Font("monospaced",Font.BOLD,16));
-        // ImageIcon icon2 = new ImageIcon(ClassLoader.getSystemResource("university/management/system/icons/icon2.png"));
-        // Image image2 = icon2.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
-        // m2.setIcon(new ImageIcon(image2));
-        m2.setMnemonic('B');
-        //m2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
-        m2.setBackground(Color.WHITE);
-    
-        
-        //m1.addActionListener(this);
-        //m2.addActionListener(this);
-
-                master.add(m1);
-                master.add(m2);
-        
-                mb.add(master);
-                setJMenuBar(mb);
-                         
-        
-
-
+       
 
        // add(inputpanel1);
         setVisible(true);
