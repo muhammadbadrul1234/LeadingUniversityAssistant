bf06630f4da6faaedd7cf762d5661b3ed53991a6
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 06:59:32 +0600

    Commit By Muhammad Badrul

--- src/BackendClass/StudentDetails.java
+++ src/BackendClass/StudentDetails.java
@@ -111,7 +111,7 @@ public class StudentDetails extends JFrame implements ActionListener{
             try{
                 String a = t2.getText();
                 String q = "delete from student where rollno = '"+a+"'";
-                c1.s.executeUpdate(q);
+                db.s.executeUpdate(q);
                 this.setVisible(false);
                 new StudentDetails().setVisible(true);
             }catch(Exception e){}
@@ -123,9 +123,6 @@ public class StudentDetails extends JFrame implements ActionListener{
             //new UpdateStudent().setVisible(true);
             this.setVisible(false);
         }
-    }
-    public static void main(String[] args){
-        new StudentDetails().setVisible(true);
     }
     
 }
