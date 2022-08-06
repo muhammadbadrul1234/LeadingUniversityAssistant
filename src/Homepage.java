f93f6b5663d91b23122469ca39efd3671d2ff901
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-06 06:19:47 +0600

    Commit By Muhammad Badrul

--- src/Homepage.java
+++ src/Homepage.java
@@ -151,13 +151,16 @@ public class Homepage extends JFrame {
                         panel.setBounds(0, 0, 500, 500);
                         frame.add(panel);
 
-                                        l1 = new JLabel("Username");
+                        JLabel l1,l2;
+                        JTextField t1;
+                        JPasswordField t2;
+                        JButton b1,b2;
+
+                        l1 = new JLabel("Student ID");
                         l1.setBounds(40,20,100,30);
                         add(l1);
                         
-                        l2 = new JLabel("Password");
-                        l2.setBounds(40,70,100,30);
-                        add(l2);
+                       
                 
                         t1=new JTextField();
                         t1.setBounds(150,20,150,30);
